2f6cc6b0590ecfa677ad2170a7042f01962a472a
AndreSalas <andre.salas.cr.201@gmail.com>
2025-07-02 00:45:51 -0600

    Se empieza el nivel 3

---
+++ Engines/game2/game2/game2/assets/maps/map03/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1-99-gec89c545" name="tileset" tilewidth="16" tileheight="16" tilecount="5696" columns="89">
+ <image source="../../images/tileset2.png" width="1424" height="1024"/>
+</tileset>
